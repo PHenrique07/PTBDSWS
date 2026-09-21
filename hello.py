8592a7a5f5ccd3d97742a31e3aafb699c8ba5e4b
Pedro Henrique Santos da Silva <pedroph1601@gmail.com>
2026-09-20 23:25:52 -0300

    fix: correção do load do dot env

--- hello.py
+++ hello.py
@@ -11,9 +11,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 # Carrega as variáveis do arquivo .env criado localmente (pra n dar ban)
-load_dotenv() 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
